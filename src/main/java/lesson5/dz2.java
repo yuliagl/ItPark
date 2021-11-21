c5c2e21b029c8581c0d1567a18c985a7ca6bad5c
Никита <yugtva@gmail.com>
2021-11-21 19:02:38 +0400

    add

---
+++ src/main/java/lesson5/dz2.java
@@ -0,0 +1,16 @@
+package lesson5;
+
+public class dz2 {
+    public static void main(String[] args) {
+        int a = 0;
+        int num = 1;
+        int b = 0;
+        //Scanner scanner = new Scanner(System.in);
+        for (; ; ) {
+            b = a;
+            a = num;
+            num = b + num;
+            System.out.println(num);
+        }
+    }
+}
